133e2e74b32f121949fa6a128cebe7f4546640a7
KamalKirosingh <kamal.kirosingh16@gmail.com>
2020-09-05 21:11:08 +0100

    Created the bird object plus the initial structure of the game.

--- src/flappyBird/FlappyBird.java
+++ src/flappyBird/FlappyBird.java
@@ -1,17 +1,28 @@
 package flappyBird;
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
 import javax.swing.*;
 
 public class FlappyBird implements ActionListener {
     public static FlappyBird flappyBird;
-    public final int WIDTH = 800, HEIGHT = 800;
-    public Renderer renderer = new Renderer();
+
+    public final int WIDTH = 600, HEIGHT = 600;
+
+    public Renderer renderer;
+
+    public Random rand = new Random();
+
+    public Rectangle bird = new Rectangle(WIDTH / 2-10, HEIGHT / 2 - 10, 20, 20);
+
+    public ArrayList<Rectangle> columns = new ArrayList<>();
 
     public FlappyBird() {
         JFrame jframe = new JFrame();
         Timer timer = new Timer(20, this);
+        renderer = new Renderer();
 
         jframe.add(renderer);
 
@@ -29,7 +40,25 @@ public class FlappyBird implements ActionListener {
         renderer.repaint();
     }
 
+
+
+    public void paintColumn(Graphics g, Rectangle column) {
+        g.setColor(Color.GREEN.darker());
+        g.fillRect(column.x, column.y, column.width, column.height);
+    }
+
     public void repaint(Graphics g) {
+        g.setColor(Color.CYAN);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        g.setColor(Color.ORANGE);
+        g.fillRect(0, HEIGHT - 120, WIDTH, 100);
+
+        g.setColor(Color.GREEN);
+        g.fillRect(0, HEIGHT - 120, WIDTH, 20);
+
+        g.setColor(Color.RED);
+        g.fillRect(bird.x, bird.y, bird.width, bird.height);
 
     }
     public static void main(String[] args) {
--- src/flappyBird/Renderer.java
+++ src/flappyBird/Renderer.java
@@ -10,7 +10,7 @@ private static final long serialVersionID = 1L;
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        FlappyBird.flappyBird.repaint(g);
+         FlappyBird.flappyBird.repaint(g);
     }
 
 }
